2a58f02a8d308c1bc40661214ea8edcb302103ae
RashedujjamanNoor <noorrk042@gmail.com>
2025-07-08 21:34:40 +0600

    create exclusiveOffer and testimonials components and showing to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
 import FeaturedDestination from "../components/FeaturedDestination ";
+import ExclusiveOffer from "../components/ExclusiveOffer";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffer />
+      <Testimonial />
     </>
   );
 };

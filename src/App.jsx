5b46b1f8dec75af5c2848bbb4bddf93aa9797199
RashedujjamanNoor <noorrk042@gmail.com>
2025-07-18 22:53:46 +0600

    remove provider from main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
@@ -11,12 +11,16 @@ import Layout from "./pages/hotelOwner/Layout";
 import Dashboard from "./pages/hotelOwner/Dashboard";
 import AddRoom from "./pages/hotelOwner/AddRoom";
 import ListRoom from "./pages/hotelOwner/ListRoom";
+import { Toaster } from "react-hot-toast";
+// import { AppContext } from "./context/AppContext";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
+  // const { showHotelReg } = useContext(AppContext);
 
   return (
     <div>
+      <Toaster />
       {!isOwnerPath && <Navbar />}
       {false && <HotelReg />}
       <div>
